3f27a4470241a0752082e1eebded59f02292c6db
abiyudaA <yudaabiabdulhaq@gmail.com>
2025-03-04 21:43:26 +0700

    feat: client tetris setup

--- client/src/views/Playground.jsx
+++ client/src/views/Playground.jsx
@@ -1,2 +1,17 @@
 
 
+export default function PlayGround() {
+
+    return (
+        <>
+      <div className='flex flex-row justify-center'>
+          <div>
+            <User1/>
+          </div>
+          <div>
+            <User2/>
+          </div>
+      </div>
+    </>
+    )
+}
