5e1cbda7d5a7e86e5271aececfaf15c6910db0a7
Demas janitro <demasfjanitro@gmail.com>
2025-03-05 22:43:37 +0700

    styling layouting modal

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,27 @@
-import { useNavigate } from "react-router"
-
+import { useNavigate } from 'react-router';
 
 export default function Navbar() {
-    const navigate = useNavigate()
-    function handleLogout(){
-        localStorage.clear()
-        navigate('/')
+  const navigate = useNavigate();
 
-    }
+  function handleLogout() {
+    localStorage.clear();
+    navigate('/');
+  }
 
-    return (
-        <div className="bg-gray-900 text-white">
-
-            <nav className="bg-gray-800 p-4 flex justify-between items-center">
-                <div className="flex-grow text-center">
-                    <span className="font-bold text-xl">TEZTRIZ</span>
-                </div>
-                <div>
-                    <button className="bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded" onClick={handleLogout}>
-                        Logout
-                    </button>
-                </div>
-            </nav>
-        </div>
-
-
-    )
+  return (
+    <nav className="z-10 bg-gray-800 fixed top-0 w-full p-4 flex justify-between items-center shadow-md">
+      <div className="flex items-center">
+        <span className="text-gray-200 font-bold text-xl font-mono">
+          TEZTRIZ
+        </span>
+      </div>
+      <div className="flex items-center">
+        <button
+          className="bg-red-700 hover:bg-red-600 text-gray-200 font-bold py-2 px-4 rounded"
+          onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
 }
--- client/src/views/Playground.jsx
+++ client/src/views/Playground.jsx
@@ -1,46 +1,31 @@
-import { useEffect } from "react"
-import Chatbox from "../components/Chatbox"
-import Navbar from "../components/Navbar"
-import User1 from "../components/User1"
-import User2 from "../components/User2"
-import { useNavigate } from "react-router"
-import { socket } from "../socket/socket"
-
+import { useEffect } from 'react';
+import Chatbox from '../components/Chatbox';
+import User1 from '../components/User1';
+import { useNavigate } from 'react-router';
+import { socket } from '../socket/socket';
 
 export default function PlayGround() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if(!localStorage.username){
-      navigate('/')
+    if (!localStorage.username) {
+      navigate('/');
     }
 
-    socket.emit("playground", 'tes')
+    socket.emit('playground', 'tes');
 
     return () => {
-        socket.off("message:update")
-        socket.disconnect()
-    }
-},[])
-
-
+      socket.off('message:update');
+      socket.disconnect();
+    };
+  }, []);
 
   return (
-    <>
-      <Navbar />
-
-      <div>
-        <div className='flex flex-row justify-center gap-20'>
-          <div className="bg-white">
-            <User1 />
-          </div>
-          <div className="bg-white">
-            <User2 />
-          </div>
-            <Chatbox />
-        </div>
+    <div className="fixed container mx-auto p-4 h-screen bg-gray-900 text-gray-200 mt-16">
+      <div className="bg-gray-800 rounded-lg shadow-md p-4">
+        <User1 />
       </div>
-
-    </>
-  )
+      <Chatbox />
+    </div>
+  );
 }
